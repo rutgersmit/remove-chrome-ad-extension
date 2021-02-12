acd6b11249a60714c10a68874c09a1665f504f65
Rutger <rutger.smit@gmail.com>
2021-02-12 22:48:50 +0100

    v 0.2
    Improved detection and application specific selectors

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Remove Chrome ad extension
 Removes the annoying popup (that keeps coming back) on Google sites that suggests to use Google Chrome.
+
+After 10 pageloads it stops temporarily. The count is registered in the cookie called OGPC.
--- src/content.js
+++ src/content.js
@@ -1,8 +1,23 @@
-if (document.title.indexOf("Google") != -1) {
-  setTimeout(function(){ 
-    var el = document.querySelector("#gbw > div > div > div:nth-child(3)");
-    if(el){
-      el.remove();
-    }
-  }, 1000);
+checkSjit();
+
+async function checkSjit(){
+  var i=0;
+  while(i++<10){
+    setTimeout(function(){removeIt();}, 1000);
+    await sleep(500);
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function removeIt(){
+  var fs = document.getElementsByTagName('iframe');
+  for (var i = 0; i < fs.length; i++) {
+    var s =  fs[i].attributes.src;
+    var v = s ? s.value : '';
+    if(v.indexOf('https://ogs.google.com/u/0/widget/callout')==0)
+    fs[i].parentElement.remove();
+	}
 }
